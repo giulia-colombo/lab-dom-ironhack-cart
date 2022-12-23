0fe828ef08594e8c6a08190b968787d4729855eb
Giulia Colombo <giulia.colombo.127@gmail.com>
2022-12-23 16:54:11 +0100

    complete iterations 0-3

--- js/index.js
+++ js/index.js
@@ -1,30 +1,50 @@
+const calculatePricesBtn = document.getElementById('calculate');
+const totalCartValue = document.querySelector('#total-value span')
+
 // ITERATION 1
 
 function updateSubtotal(product) {
+
   console.log('Calculating subtotal, yey!');
   const price = product.querySelector(".price span");
   const priceValue = price.innerText;
   const quantity = product.querySelector(".quantity input");
-  const quantityValue = quantity.value;
- 
+  const quantityValue = quantity.value; //returns the value prop/attr of quantity
   const subTotalValue = priceValue * quantityValue;
+  const subTotal = product.querySelector(".subtotal span");//not sure about .document
+  // console.log(subTotal);
+  //set the subtotal price to the corresponding DOM element
+  subTotal.innerText = subTotalValue;
+  // console.log(subTotalValue);
+  return subTotalValue;
 
-  const subTotal = subtotal.querySelector()
-
-  //... your code goes here
 }
 
 
 function calculateAll() {
+  //calculateAll() triggers the update of subtotals for every product
+
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-
-  // end of test
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
 
   // ITERATION 2
-  //... your code goes here
+  const products = document.getElementsByClassName("product");
+  let total = 0;
+  for (let i = 0; i < products.length; i++) {
+    const productSubTotal = ((updateSubtotal(products[i])));
+    //sum the subtotals for each product
+    total += productSubTotal;
+  };
+  console.log(total);
+  //display this value in the total DOM element
+    //grab the corresponding element in the dom
+    //update its inner value
+  totalCartValue.innerText = total;
+
+  
+ 
 
   // ITERATION 3
   //... your code goes here
@@ -45,7 +65,6 @@ function createProduct() {
 }
 
 window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
